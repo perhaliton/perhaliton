615b0b10aa47770d37972a3ef9c2133345559fab
perhaliton <perhaliton@Admins-MacBook-Air.local>
2023-08-04 03:20:09 +0800

    first commit

---
+++ jekyll-datapage-generator.js
@@ -0,0 +1,10 @@
+Gem::Specification.new do |s|
+  s.name      = 'jekyll-datapage-generator'
+  s.metadata  = { 'source_code_uri' => 'https://github.com/avillafiorita/jekyll-datapage_gen' }
+  s.version   = '1.4.0'
+  s.summary   = 'Generate one page per yaml record in Jekyll sites.'
+  s.licenses  = ['MIT']
+  s.authors   = ['Adolfo Villafiorita']
+  s.email     = ['adolfo.villafiorita@ict4g.net']
+  s.files     = ['lib/jekyll-datapage-generator.rb']
+end
